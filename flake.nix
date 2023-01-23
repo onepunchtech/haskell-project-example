5f7ebe3e60c93a7d78b52401cc37fb2793c7d2b4
Michael Whitehead <onepunchlinux@gmail.com>
2023-01-22 19:56:45 -0700

    initial commit

---
+++ flake.nix
@@ -0,0 +1,62 @@
+{
+  description = "haskell project workshop";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs?rev=2117c50988e796dc76bab4b5fc9dab84dbb91098";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        hPkgs =
+          pkgs.haskell.packages."ghc925";
+
+        myDevTools = [
+          hPkgs.ghc
+          hPkgs.ghcid
+          hPkgs.ormolu
+          hPkgs.hlint
+          hPkgs.hoogle
+          hPkgs.haskell-language-server
+          hPkgs.implicit-hie
+          hPkgs.retrie
+          hPkgs.hspec-discover
+          stack-wrapped
+          pkgs.zlib
+          pkgs.tree-sitter
+          pkgs.nodejs
+          pkgs.yarn
+          pkgs.kube3d
+          pkgs.kubernetes-helm
+          pkgs.skopeo
+          pkgs.telepresence2
+          pkgs.typescript
+          pkgs.nodePackages.typescript
+          pkgs.nodePackages.typescript-language-server
+          pkgs.typescript
+        ];
+
+        stack-wrapped = pkgs.symlinkJoin {
+          name = "stack";
+          paths = [ pkgs.stack ];
+          buildInputs = [ pkgs.makeWrapper ];
+          postBuild = ''
+            wrapProgram $out/bin/stack \
+              --add-flags "\
+                --no-nix \
+                --system-ghc \
+                --no-install-ghc \
+              "
+          '';
+        };
+
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = myDevTools;
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath myDevTools;
+        };
+
+        packages = {
+        };
+      });
+}
